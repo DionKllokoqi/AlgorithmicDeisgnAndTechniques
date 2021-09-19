627ed239cd87dc260c069e8242b9f467cb4425bb
dion <dion.kllokoqi@gmail.com>
2021-09-19 21:17:54 +0200

    Adds Coin Change Dynamic Algorithm.

---
+++ week5/change_dp.py
@@ -0,0 +1,27 @@
+# Uses python3
+import sys
+
+def get_change(m):
+    
+    # List will hold min number of change coins for values 0 - m
+    min_num_coins = [0] * (m + 1)
+    # Change coins
+    coins = [1, 3, 4]
+
+    # Looping through all the money values from 0 to m to find the change for each
+    for money in range(1, m + 1):
+        min_num_coins[money] = float('inf')
+        # For each coin, we calculate the change we would get
+        for coin in coins:
+            # If current coin is larger than current money value, we cannot get change
+            if money >= coin:
+                # We add + 1 because we need to account for when (money - coin) = 0 and get_change rets 0
+                num_of_coins = min_num_coins[money - coin] + 1
+                if num_of_coins < min_num_coins[money]:
+                    min_num_coins[money] = num_of_coins
+    
+    return min_num_coins[m]
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
---
+++ week5/change_dp_stresstests.py
@@ -0,0 +1,12 @@
+import sys
+import change_dp
+
+def change_dp_test():
+    m = 0
+
+    while True:
+        print(f"Change for m: {m} = {change_dp.get_change(m)}")
+        m += 1
+
+if __name__ == '__main__':
+    change_dp_test()
