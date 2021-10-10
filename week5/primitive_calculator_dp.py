1131b235620b2564804078a5da5c03ff23854308
dion <dion.kllokoqi@gmail.com>
2021-10-10 19:20:13 +0200

    [+] Finished Primitive Calculator.

--- week5/primitive_calculator_dp.py
+++ week5/primitive_calculator_dp.py
@@ -3,45 +3,44 @@ import sys
 
 def optimal_sequence(n):
 
-    # Intermediate sequence to arrive at n
-    optimal_sequence = [1]
-    
+    parents = [None] * (n + 1)
     # Sequence of number of operations to get to each number from 1 to n
-    min_num_of_operations = [0] * (n + 1)
+    min_num_of_operations = [0] + [float('inf')] * n
 
-    # For each number, find the optimal way to get there via
-    # a calculation with one of the operators
-    if n > 1:
-        min_num_of_operations[float('inf')] = -1
+    # Find the optimal solution for each number from 1 to n
+    for i in range(1, n + 1):
+        current_parent = i - 1
+        current_min_op = min_num_of_operations[current_parent] + 1
 
-        # Find the optimal solution for each number from 1 to n
-        for i in range(1, n + 1):
-            num_of_operations_mult3 = float('inf')
-            num_of_operations_mult2 = float('inf')
-            num_of_operations_add1  = float('inf')
+        # If i is divisible by 3, we can get to the current
+        # i from i // 3 by multiplying it with 3
+        # => steps to get to i = steps to get to i // 3 + 1
+        if (i % 3) == 0:
+            parent = i // 3
+            num_ops = min_num_of_operations[parent] + 1
+            if num_ops < current_min_op:
+                current_min_op, current_parent = num_ops, parent
 
-            # If i is divisible by 3, we can get to the current
-            # i from i // 3 by multiplying it with 3
-            # => steps to get to i = steps to get to i // 3 + 1
-            if (i % 3) == 0:
-                num_of_operations_mult3 = min_num_of_operations[i // 3] + 1
+        # If i is divisible by 2, we can get to the
+        # current i from i // 2 by multiplying it with 2
+        # => steps to get to i = steps to get to i // 2 + 1
+        if (i % 2) == 0:
+            parent = i // 2
+            num_ops = min_num_of_operations[parent] + 1
+            if num_ops < current_min_op:
+                current_min_op, current_parent = num_ops, parent
 
-            # If i is divisible by 2, we can get to the
-            # current i from i // 2 by multiplying it with 2
-            # => steps to get to i = steps to get to i // 2 + 1
-            if (i % 2) == 0:
-                num_of_operations_mult2 = min_num_of_operations[i // 2] + 1
+        min_num_of_operations[i], parents[i] = current_min_op, current_parent
+    
+    k = n
+    reversed_sequence = []
+    while k > 0:
+        current_parent = k
+        reversed_sequence.append(current_parent)
+        k = parents[k]
 
-            # We can always get to the current i from the previous i-1 via one extra step
-            num_of_operations_add1 = min_num_of_operations[i - 1] + 1
-
-            # Check which is the optimal way
-            min_num_of_operations[i] = min(num_of_operations_mult3,
-                                          num_of_operations_mult2,
-                                          num_of_operations_add1)
-
-    optimal_sequence.append(n)
-    return optimal_sequence
+    reversed_sequence.reverse()
+    return reversed_sequence
 
 inputVal = sys.stdin.read()
 n = int(inputVal)
