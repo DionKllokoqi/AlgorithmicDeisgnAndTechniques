79e3b0da132b37eafab9fb67b918ce97c36817cf
Dion Kllokoqi <dion.kllokoqi@gmail.com>
2023-10-17 22:34:01 +0200

    test: finish pariwise product tests

--- week1/pairwise_product.py
+++ week1/pairwise_product.py
@@ -1,3 +1,6 @@
+# Uses python3
+
+
 def get_maximum_pairwise_product(numbers):
     largest, largest_index = -1, 0
     for i in range(len(numbers)):
@@ -11,3 +14,9 @@ def get_maximum_pairwise_product(numbers):
             second_largest = numbers[i]
 
     return largest * second_largest
+
+
+if __name__ == "__main__":
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(get_maximum_pairwise_product(input_numbers))
