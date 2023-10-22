275706ffe08d286b0294b51e5e5e803910657bce
Dion Kllokoqi <dion.kllokoqi@gmail.com>
2023-10-22 22:05:03 +0200

    chore: format week1 code

--- week1/stress_test.py
+++ week1/stress_test.py
@@ -1,12 +1,10 @@
 from random import *
 
-def StressTest(N, M):
 
+def StressTest(N, M):
     while True:
         n = randint(2, N)
         a = [None] * n
 
         for i in range(n):
             a[i] = randint(0, M)
-        
-        
