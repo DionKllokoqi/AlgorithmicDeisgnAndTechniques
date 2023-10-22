8a3254ea9be6c04fdbbbb95862db437c17f484b2
Dion Kllokoqi <dion.kllokoqi@gmail.com>
2023-10-22 22:26:52 +0200

    test: add stress test for pairwise product

--- week1/naive_pairwise_product.py
+++ week1/naive_pairwise_product.py
@@ -1,11 +1,18 @@
 # Uses python3
-n = int(input())
-a = [int(x) for x in input().split()]
 
-product = 0
 
-for i in range(n):
-    for j in range(i + 1, n):
-        product = max(product, a[i] * a[j])
+def get_maximum_pairwise_product_naive(n, a):
+    product = 0
 
-print(product)
+    for i in range(n):
+        for j in range(i + 1, n):
+            product = max(product, a[i] * a[j])
+
+    return product
+
+
+if __name__ == "__main__":
+    n = int(input())
+    a = [int(x) for x in input().split()]
+
+    print(get_maximum_pairwise_product_naive(n, a))
--- week1/stress_test.py
+++ week1/stress_test.py
@@ -1,4 +1,6 @@
 from random import *
+from naive_pairwise_product import get_maximum_pairwise_product_naive
+from pairwise_product import get_maximum_pairwise_product
 
 
 def StressTest(N, M):
@@ -8,3 +10,20 @@ def StressTest(N, M):
 
         for i in range(n):
             a[i] = randint(0, M)
+
+        print(a)
+
+        result1 = get_maximum_pairwise_product_naive(n, a)
+        result2 = get_maximum_pairwise_product(a)
+
+        if result1 == result2:
+            print("OK")
+        else:
+            print("Wrong answer: ", result1, result2)
+            return
+
+
+if __name__ == "__main__":
+    n = int(input())
+    m = int(input())
+    StressTest(n, m)
