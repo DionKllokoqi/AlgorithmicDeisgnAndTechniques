b11b48a766cd827ca75f7b4f5cf2e4c25f2d70c4
Dion Kllokoqi <dion.kllokoqi@gmail.com>
2024-01-05 17:05:55 +0100

    fix: import issues

--- tests/week2/fibonacci/lastDigit/fibonacci_last_digit_stress_tests.py
+++ tests/week2/fibonacci/lastDigit/fibonacci_last_digit_stress_tests.py
@@ -1,12 +1,12 @@
 # Uses python3
 import sys
-from fibonacci_last_digit_naive import (
-    get_fibonacci_last_digit_naive as get_last_fib_digit_naive,
-)
-from fibonacci_last_digit_improved import (
+
+from src.week2.fibonacci.lastDigit.fibonacci_last_digit_improved import (
     get_fibonacci_last_digit_improved as get_last_fib_digit,
 )
-
+from src.week2.fibonacci.lastDigit.fibonacci_last_digit_naive import (
+    get_fibonacci_last_digit_naive as get_last_fib_digit_naive,
+)
 
 if __name__ == "__main__":
     input = sys.stdin.read()
